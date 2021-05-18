3f48744d141deb98d1633a29e11d8e7af353e93e
Larry Hodges <74116412+imhotep-hodges@users.noreply.github.com>
2021-05-17 20:14:37 -0400

    adding styles

--- src/App.js
+++ src/App.js
@@ -2,27 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import '/Users/larryh1981/project05/src/index.css';
 import Navbar from "/Users/larryh1981/project05/src/components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>     
+      <Navbar />
+      
+    </Router>
   );
-}
+};
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
-
-
+import { Link } from 'react-router-dom';
 
 
 
@@ -13,24 +11,27 @@ function Navbar() {
             <div className="nav-container">
             
                 <h1 className="navBrand">
-                Larry's ToDo App! 
+                Project #5 
                 </h1>
                 
-                <ul className='nav-menu'>
+                <ul className="btn-group-vertical" role="group" aria-label="Basic example">
+
                     <li className="nav-item">
-                       
-                       Home
-                       
+                    <Link to="#" className="nav-links"> 
+                      <button type="button" class="btn btn-danger btn-lg">Question #1</button>
+                    </Link>
                     </li>
+
                     <li className="nav-item">
-                       
-                       Category 1
-                      
+                    <Link to="#" className="nav-links">  
+                    <button type="button" class="btn btn-danger btn-lg">Question #2</button>
+                    </Link>
                     </li>
+
                     <li className="nav-item">
-                       
-                       Category 2
-                   
+                    <Link to="#" className="nav-links">   
+                    <button type="button" class="btn btn-danger btn-lg">Question #3</button>
+                    </Link>
                     </li>               
                 </ul>
 
