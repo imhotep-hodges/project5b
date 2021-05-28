539eb7ae5e652e794e788c3973caff749bf09d70
Larry Hodges <74116412+imhotep-hodges@users.noreply.github.com>
2021-05-27 22:41:20 -0400

    adding routes

--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser'; 
 import mongoose from 'mongoose';
 import cors from 'cors';
+import express, { json } from 'express';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "10mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: true}));
+app.use(json());
 app.use(cors());
 
 const CONNECTION_URL = "mongodb+srv://larryhadmin:Bingo2021@cluster0.iihlr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true });
+mongoose.connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
     .catch((error) => console.log(error.message));
 
