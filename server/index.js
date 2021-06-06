fd86d7a9ae035e6eee2133cdc01a51e6df7c3e6c
Larry Hodges <74116412+imhotep-hodges@users.noreply.github.com>
2021-06-06 12:30:21 -0400

    saving

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+
 const CONNECTION_URL = "mongodb+srv://larryhadmin:Bingo2021@cluster0.iihlr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
